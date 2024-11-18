d5222efe42e9f7d2ed5ae616868cda2671d35e25
ankit3131 <83747038+ankit3131@users.noreply.github.com>
2024-11-18 07:31:06 +0530

    Done

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ function checkScrollAndRemoveClass() {
 window.addEventListener("scroll", checkScrollAndRemoveClass);
 
 
-
 ///scrolling use work
 function animateOnScroll() {
   const animatedElements  = document.querySelectorAll('.fade-left, .fade-right, .zoom-in,.zoom-out ,.fade-in-up, .fade-in-down ,.fade-rotate');
@@ -72,3 +71,59 @@ button.addEventListener('mouseleave' , ()=>{
   button.classList.add('expand');
 })
 });
+
+
+// form Valdation use
+
+const form = document.querySelector('#contactForm');
+let names = document.getElementById('name');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+let phone = document.getElementById('emails');
+let textError = document.querySelectorAll('.textError');
+
+function nameValdation(e) {
+e.preventDefault();
+console.log('alert')
+textError.forEach( (Element , index) =>{
+  Element.textContent = "";
+}
+
+)
+////
+if(names.value.trim() === ""){
+  debugger;
+textError[0].textContent = "Please Enter Name";
+}
+else {
+  textError[0].textContent = "";
+ names.classList.add("success"); 
+}
+
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+if(email.value.trim() === ""){
+  textError[1].textContent = "Please fill up";
+}
+else if(!emailPattern.test(email.value.trim())){
+  debugger;
+textError[1].textContent = "email not valid";
+}
+else{
+  textError[1].textContent = "";
+}
+if(phone.value.trim() === "") {
+  textError[2].textContent = "Please Enter Number";
+}
+else {
+  textError[2].textContent = "";
+}
+if(message.value.trim() === "") {
+  textError[3].textContent = "Please review";
+}
+else {
+  textError[3].textContent = "";
+}
+}
+
+form.addEventListener('submit' , nameValdation);
